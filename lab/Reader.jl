ee6228881690f15b56458a53ce9e32a31450e4b9
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-09-20 08:38:18 -0300

    input data dir

--- lab/Reader.jl
+++ lab/Reader.jl
@@ -8,15 +8,15 @@ export read_config, read_ena
 
 CONFIG_FILENAME = "config.json"
 ENA_FILENAME = "ena.csv"
-OUTDIR = "./out"
+INDIR = "./data"
 
 function read_config()::ConfigData
-    return from_json(JSON.parsefile(CONFIG_FILENAME))
+    return from_json(JSON.parsefile(joinpath(INDIR, CONFIG_FILENAME)))
 end
 
 function read_ena()::Dict{Int,Vector{Float64}}
     return Dict((e.MES, [e.MEDIA, e.DESVIO])
-                for e in CSV.File(ENA_FILENAME))
+                for e in CSV.File(joinpath(INDIR, ENA_FILENAME)))
 end
 
 end
