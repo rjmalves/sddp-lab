90b9c46ff7f0ee342796a33ed8a21a31356bf580
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-09-20 23:59:54 -0300

    Adding package compiler

--- main.jl
+++ main.jl
@@ -1,20 +1,12 @@
-using Comonicon
-
 include("lab/Lab.jl")
 
-"""
-    Julia CLI for running SDDP studies.
-"""
-@main function sddp_lab()
-    cfg = Lab.read_config()
-    ena = Lab.read_ena()
-    model = Lab.build_model(cfg, ena)
-    Lab.train_model(model, cfg)
-    cuts = Lab.write_model_cuts(model)
-    Lab.plot_model_cuts(cuts, cfg)
+cfg = Lab.read_config()
+ena = Lab.read_ena()
+model = Lab.build_model(cfg, ena)
+Lab.train_model(model, cfg)
+cuts = Lab.write_model_cuts(model)
+Lab.plot_model_cuts(cuts, cfg)
 
-    sims = Lab.simulate_model(model, cfg)
-    Lab.write_simulation_results(sims)
-    Lab.plot_simulation_results(sims, cfg)
-
-end
+sims = Lab.simulate_model(model, cfg)
+Lab.write_simulation_results(sims)
+Lab.plot_simulation_results(sims, cfg)
