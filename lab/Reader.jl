4024ec70b232f22aa47c245fe8098ef46958a9d0
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2023-06-05 13:37:53 -0300

    read_ena suporta multiplas UHEs em ena.csv

--- lab/Reader.jl
+++ lab/Reader.jl
@@ -2,6 +2,7 @@ module Reader
 
 using JSON
 using CSV
+using DataFrames
 using Logging
 using ..Config
 
@@ -18,10 +19,19 @@ function read_config()::ConfigData
     return ConfigData(JSON.parsefile(CONFIG_PATH))
 end
 
-function read_ena()::Dict{Int,Vector{Float64}}
+function read_ena()::Dict{Int,Dict{Int, Vector{Float64}}}
     @info "Lendo arquivo de configuração $(ENA_PATH)"
-    return Dict((e.MES, [e.MEDIA, e.DESVIO])
-                for e in CSV.File(ENA_PATH))
+    dat_ena = CSV.read(ENA_PATH, DataFrame)
+    uhes = unique(dat_ena[:, :UHE])
+    
+    out = Dict()
+    for u in uhes
+        aux = Dict((e.MES, [e.MEDIA, e.DESVIO])
+                for e in CSV.File(ENA_PATH) if e.UHE == u)
+        out[u] = aux
+    end
+
+    return out
 end
 
 end
