de6141f313244293bcd05bceb4e4405b743cdaef
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-06-06 15:21:33 -0300

    Corrige chamada de write_sim_res

--- main.jl
+++ main.jl
@@ -8,7 +8,7 @@ model = Lab.build_model(cfg, ena)
 Lab.train_model(model, cfg)
 
 sims = Lab.simulate_model(model, cfg)
-Lab.write_simulation_results(sims)
+Lab.write_simulation_results(sims, cfg)
 Lab.plot_simulation_results(sims, cfg)
 
 cuts = Lab.write_model_cuts(model)
