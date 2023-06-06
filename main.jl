219bae38976ef47511bf78ee52d61fb2e7e7394f
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-06-06 15:03:01 -0300

    Deixa plot dos cortes pro fim

--- main.jl
+++ main.jl
@@ -1,16 +1,15 @@
 ENV["GKSwstype"] = "100"
 
-
 include("lab/Lab.jl")
-
 
 cfg = Lab.read_config()
 ena = Lab.read_ena()
 model = Lab.build_model(cfg, ena)
 Lab.train_model(model, cfg)
-cuts = Lab.write_model_cuts(model)
-Lab.plot_model_cuts(cuts, cfg)
 
 sims = Lab.simulate_model(model, cfg)
 Lab.write_simulation_results(sims)
 Lab.plot_simulation_results(sims, cfg)
+
+cuts = Lab.write_model_cuts(model)
+Lab.plot_model_cuts(cuts, cfg)
