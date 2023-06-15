5544bd2968b694c01713a0bad0a59f6aa87bf795
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-06-15 12:19:48 -0300

    Add docstrings as classes e funs de Config

--- lab/Config.jl
+++ lab/Config.jl
@@ -2,6 +2,21 @@ module Config
 
 export ConfigData
 
+"""
+   UHEConfigData
+
+Classe contendo informacoes sobre uma UHE
+
+Atributos da classe
+
+ * `index::Int`: indice da UHE no sistema
+ * `ghmin::Float64`: geracao minima
+ * `ghmax::Float64`: geracao maxima
+ * `earmin::Float64`: energia armazenada minima
+ * `earmax::Float64`: energia armazenada maxima
+ * `initial_ear::Float64`: energia armazenada inicial
+ * `spill_penal::Float64`: penalidade de vertimento
+"""
 struct UHEConfigData
     name::String
     ghmin::Float64
@@ -12,27 +27,79 @@ struct UHEConfigData
     spill_penal::Float64
 end
 
+"""
+    ParqueUHEConfigData
+
+Classe contendo informacoes do parque hidroeletrico do sistema
+
+Atributos da classe
+
+ * `n_uhes::Int`: numero de UHEs no sistema
+ * `uhes::Vector{UHEConfigData}`: vetor de objetos `UHEConfigData`
+"""
 struct ParqueUHEConfigData
     n_uhes::Int
     uhes::Vector{UHEConfigData}
 end
 
+"""
+    ParqueUHEConfigData(uhes::Vector{UHEConfigData})
+
+Constroi um `ParqueUHEConfigData` a partir de um vetor de `UHEConfigData`
+"""
 function ParqueUHEConfigData(uhes::Vector{UHEConfigData})
     n_uhes = length(uhes)
     ParqueUHEConfigData(n_uhes, uhes)
 end
 
+"""
+    UHEConfigData
+
+Classe contendo informacoes sobre uma UTE
+
+Atributos da classe
+
+ * `gtmin::Float64`: geracao minima da termica
+ * `gtmax::Float64`: geracao maxima da termica
+ * `generation_cost::Float64`: custo de geracao
+"""
 struct UTEConfigData
     gtmin::Float64
     gtmax::Float64
     generation_cost::Float64
 end
 
+"""
+    SystemConfigData
+
+Classe contendo informacoes gerais do sistema
+
+Atributos da classe
+
+ * `deficit_cost::Float64`: custo de deficit
+ * `demand::Float64`: demanda (valor unico para todo o estudo)
+"""
 struct SystemConfigData
     deficit_cost::Float64
     demand::Float64
 end
 
+"""
+    ConfigData
+
+Classe contendo informacoes totais do estudo: UHEs, UTEs e Sistema
+
+Atributos da classe
+
+ * `initial_month::Int`: mes inicial do estudo
+ * `years::Int`: numero de anos
+ * `max_iterations::Int`: maximo numero de iteracoes para construcao da politica
+ * `number_simulated_series::Int`: numero de series para a simulacao final
+ * `scenarios_by_stage::Int`: numero de aberturas backward por estagio
+ * `parque_uhe::ParqueUHEConfigData`: objeto `ParqueUHEConfigData` representando o parque hidro
+ * `ute::UTEConfigData`: objeto `UTEConfigData` representando a termica
+ * `system::SystemConfigData`: objeto `SystemConfigData` representando parametros gerais do sistema
+"""
 struct ConfigData
     initial_month::Int
     years::Int
@@ -44,7 +111,12 @@ struct ConfigData
     system::SystemConfigData
 end
 
-function ConfigData(jsondata::Dict{String,Any})::ConfigData
+"""
+    ConfigData(jsondata::Dict{String, Any})
+
+Constroi um objeto `ConfigData` a partir de um dicionario lido do json `config.json` de entrada
+"""
+function ConfigData(jsondata::Dict{String, Any})::ConfigData
     uhes = map(
         x -> UHEConfigData(
             x["NOME"],
