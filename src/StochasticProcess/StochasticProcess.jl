bcfc9ef61518a07cdc531836c3026aa608dc245f
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-05-14 19:37:23 -0300

    Init StochasticProcess module

---
+++ src/StochasticProcess/StochasticProcess.jl
@@ -0,0 +1,30 @@
+module StochasticProcess
+
+import Base: length
+
+abstract type AbstractStochasticProcess end
+
+"""
+    __get_ids(s)
+
+Return the `id`s of elements represented in a stochastic process object
+"""
+__get_ids(s::AbstractStochasticProcess)
+
+"""
+    length(s::AbstractStochasticProcess)
+
+Return the number of dimensions (elements) in a stochastic process
+"""
+length(s::AbstractStochasticProcess)
+
+"""
+    __validate(s::AbstractStochasticProcess)
+
+Return `true` if `s` is a valid instance of stochastic process; raise errors otherwise
+"""
+__validate(s::AbstractStochasticProcess)
+
+include("naive.jl")
+
+end
