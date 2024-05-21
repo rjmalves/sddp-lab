d3dd16046f33a76b0dc2c6fc1ab78d93ebafc27c
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-05-21 11:33:54 -0300

    Add dep Suppressor; hides output from basic tests

--- test/test-base-cases.jl
+++ test/test-base-cases.jl
@@ -1,19 +1,26 @@
+using Suppressor
 
 @testset "Test Cases" begin
 
     @testset "Basic" begin
-        exec = SDDPlab.read_exec("data/")
-        SDDPlab.compute_simulate_policy(exec)
+        @suppress begin
+            exec = SDDPlab.read_exec("data/")
+            SDDPlab.compute_simulate_policy(exec)
+        end
     end
 
     @testset "Periodic" begin
-        exec = SDDPlab.read_exec("data_per/")
-        SDDPlab.compute_simulate_policy(exec)
+        @suppress begin
+            exec = SDDPlab.read_exec("data_per/")
+            SDDPlab.compute_simulate_policy(exec)
+        end
     end
 
     @testset "Weekly" begin
-        exec = SDDPlab.read_exec("data_semanal/")
-        SDDPlab.compute_simulate_policy(exec)
+        @suppress begin
+            exec = SDDPlab.read_exec("data_semanal/")
+            SDDPlab.compute_simulate_policy(exec)
+        end
     end
 
 end
