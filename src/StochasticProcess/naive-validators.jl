b592cf9547e9ec5ed36568dbaecd23d9e0b990af
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-05-27 13:36:28 -0300

    Add copula content validators

--- src/StochasticProcess/naive-validators.jl
+++ src/StochasticProcess/naive-validators.jl
@@ -1,5 +1,3 @@
-# FORM VALIDATORS --------------------------------------------------------------------------
-
 # CONTENT VALIDATORS -----------------------------------------------------------------------
 
 function __validate_distribution_name(name::String)
@@ -16,6 +14,27 @@ end
 function __validate_distribution_params(name::String, params::Vector{T} where T <: Real)
     try
         __instantiate_dist(name, params)
+    catch 
+        return AssertionError("`$params` not a valid set of parameters for distribution `$name`")
+    end
+
+    return true
+end
+
+function __validate_copula_name(name::String)
+    as_symbol = Symbol(name)
+    try
+        getfield(Copulas, as_symbol) <: getfield(Copulas, :Copula)
+    catch
+        return AssertionError("$name is not a valid Copula")
+    end
+
+    return true
+end
+
+function __validate_copula_params(name::String, params::Union{Vector{T}, Matrix{Float64}} where T <: Real)
+    try
+        __instantiate_copula(name, params)
     catch 
         return AssertionError("`$params` not a valid set of parameters for distribution `$name`")
     end
