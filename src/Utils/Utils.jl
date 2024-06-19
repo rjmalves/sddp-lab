f71e2e5585c5f39a038fe80ced5932c2bf8ce12d
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-06-19 12:03:33 -0300

    Introduces module Utils with read/valid utils

---
+++ src/Utils/Utils.jl
@@ -0,0 +1,20 @@
+module Utils
+
+include("validation-utils.jl")
+include("reading-utils.jl")
+
+export 
+    __validate_keys!,
+    __validate_key_lengths!,
+    __validate_key_types!,
+    __validate_file!,
+    __parse_as_type!,
+    __try_conversion!,
+    __try_conversion!,
+    __try_conversion!,
+    __valid_name_regex_match,
+    read_jsonc,
+    read_csv,
+    __dataframe_to_dict
+
+end
