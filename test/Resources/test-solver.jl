4ecdbbad1dd8d526de0927d959080db0eef225a3
marianasnoel <marianasimoesnoel@gmail.com>
2024-10-28 17:12:24 -0300

    improves solver tests

--- test/Resources/test-solver.jl
+++ test/Resources/test-solver.jl
@@ -8,12 +8,29 @@ DICT = Dict("kind" => "CLP", "params" => Dict{String,Any}())
     @testset "clp-valid" begin
         d, e = __renew(DICT)
         @test typeof(Resources.CLP(d["params"], e)) === Resources.CLP
+        d = __modif_key(d, "params", Dict{String,Any}("PrimalTolerance" => 0.1))
+        @test typeof(Resources.CLP(d["params"], e)) === Resources.CLP
+    end
+
+    @testset "clp-invalid" begin
+        d, e = __renew(DICT)
+        d = __modif_key(d, "params", Dict{String,Any}("PrimalTolerancee" => 0.1))
+        @test Resources.CLP(d["params"], e) === nothing
     end
 
     @testset "glpk-valid" begin
         d, e = __renew(DICT)
         d = __modif_key(d, "kind", "GLPK")
         @test typeof(Resources.GLPK(d["params"], e)) === Resources.GLPK
+        d = __modif_key(d, "params", Dict{String,Any}("tol_int" => 0.1))
+        @test typeof(Resources.GLPK(d["params"], e)) === Resources.GLPK
+    end
+
+    @testset "glpk-invalid" begin
+        d, e = __renew(DICT)
+        d = __modif_key(d, "kind", "GLPK")
+        d = __modif_key(d, "params", Dict{String,Any}("tol_intt" => 0.1))
+        @test Resources.GLPK(d["params"], e) === nothing
     end
 
     @testset "highs-valid" begin
@@ -25,9 +42,10 @@ DICT = Dict("kind" => "CLP", "params" => Dict{String,Any}())
     @testset "glpk-generate-optimizer" begin
         d, e = __renew(DICT)
         d = __modif_key(d, "kind", "GLPK")
-        solver = Resources.GLPK(d, e)
-        # TODO - improve test
-        # @test Resources.generate_optimizer(solver) === Optimizer
-        @test Resources.generate_optimizer(solver) === MOI.OptimizerWithAttributes{Any,Any}
+        solver = Resources.GLPK(d["params"], e)
+        @test typeof(Resources.generate_optimizer(solver)) === MOI.OptimizerWithAttributes
+        d = __modif_key(d, "params", Dict{String,Any}("tol_int" => 0.1))
+        solver = Resources.GLPK(d["params"], e)
+        @test typeof(Resources.generate_optimizer(solver)) === MOI.OptimizerWithAttributes
     end
 end
