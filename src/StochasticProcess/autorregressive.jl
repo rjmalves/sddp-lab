afbd7524bf43b8ae336668a7744de6e7ebeff8ca
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-12-04 15:28:39 -0300

    Basic skeleton code for AR stocastic process

---
+++ src/StochasticProcess/autorregressive.jl
@@ -0,0 +1,88 @@
+
+# INTERNAL AR TYPES ------------------------------------------------------------------------
+
+abstract type AbstractARparameters end
+
+function __build_ar_parameters(d, e)
+    if length(d["models"]) == 1
+        SimpleARparameters(d, e)
+    else
+        PeriodicARparameters(d, e)
+    end
+end
+
+struct SimpleARparameters <: AbstractARparameters
+    parameters::Vector{Float64}
+    scale::Vector{Float64}
+    season::Int
+end
+
+function SimpleARparameters(d, e)
+    # __validate_ar_parameters(d, e)
+    #     - checa se tem as chaves season, coefs, res_var e scale_par e sao dos tipos certos
+
+    return if valid
+        SimpleARparameters(d["coefficients"], d["scale_parameters"], d["season"])
+    else
+        nothing
+    end
+end
+
+struct PeriodicARparameters <: AbstractARparameters
+    parameters::Dict{Int,Vector{SimpleARparameters}}
+end
+
+function PeriodicARparameters(d, e)
+    out = Vector{SimpleARparameters}()
+    for model_dict in d["models"]
+        model = SimpleARparameters(model)
+        push!(out, model)
+    end
+
+    return out
+end
+
+# SIGNAL MODEL TYPE ------------------------------------------------------------------------
+
+struct UnivariateAutoRegressive
+    id::Int
+    initialization::Vector{Float64}
+    parameters::AbstractARparameters
+end
+
+function UnivariateAutoRegressive(d::Dict{String, Any}, e::CompositeException)
+    # valid = __validate_univariateautoregressive_dict(d, e)
+    #     - chaves id, init e models existem e sao dos tipos certos
+
+    if !valid
+        return nothing
+    end
+
+    arp = __build_ar_parameters(d["models"], e)
+
+    UnivariateAutoRegressive(d["id"], d["initialization"], arp)
+end
+
+# MAIN AR TYPE -----------------------------------------------------------------------------
+
+struct AutoRegressiveStochasticProcess <: AbstractStochasticProcess
+    signal_model::Vector{UnivariateAutoRegressive}
+    noise_model::Naive
+end
+
+function AutoRegressiveStochasticProcess(d::Dict{String, Any}, e::CompositeException)
+    # valid = __validate_autoregressive_dict(d, e)
+    #     - so checa se existem as chaves "marginal_models" e "copulas" e sao Dicts
+    return if !valid
+        nothing
+    end
+
+    signal = Dict{Int,UnivariateAutoRegressive}()
+    for marginal_model in d["marginal_models"]
+        s = UnivariateAutoRegressive(marginal_model, e)
+        signal[d["id"]] = (signal, s)
+    end
+
+    noise_dict = __build_noise_naive_dict(d, e)
+    noise = Naive(d, e)
+end
