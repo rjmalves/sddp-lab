654d693efb59063db1c0804fb56c5f6413a6655c
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-12-04 22:32:00 -0300

    add_inflow_uncertainty! method for AR

--- src/StochasticProcess/autoregressive.jl
+++ src/StochasticProcess/autoregressive.jl
@@ -57,6 +57,8 @@ function UnivariateAutoRegressive(d::Dict{String, Any}, e::CompositeException)
     end
 
     arp = __build_ar_parameters(d, e)
+    
+    # TODO: validate that init is same size as maximum lag in models
 
     UnivariateAutoRegressive(d["id"], d["initial_values"], arp)
 end
@@ -128,6 +130,50 @@ function __get_lag(ar::UnivariateAutoRegressive)
     __get_lag(ar.parameters)
 end
 
+function __get_ar_parameters(ar::SimpleARparameters)
+    ar.parameters
+end
+
+function __get_ar_parameters(ar::SimpleARparameters, ::Int)
+    __get_ar_parameters(ar)
+end
+
+function __get_ar_parameters(ar::PeriodicARparameters, season::Int)
+    seasons = map(x -> x.season, ar.parameters)
+    index = findfirst(x -> x == season, seasons)
+    __get_ar_parameters(ar.parameters[index])
+end
+
+function __get_ar_parameters(uar::UnivariateAutoRegressive, season::Int)
+    __get_ar_parameters(uar.parameters, season)
+end
+
+function __get_ar_parameters(s::AutoRegressiveStochasticProcess, season::Int)
+    [__get_ar_parameters(uar, season) for uar in s.signal_model]
+end
+
+function __get_ar_scale(ar::SimpleARparameters)
+    ar.scale
+end
+
+function __get_ar_scale(ar::SimpleARparameters, ::Int)
+    __get_ar_scale(ar)
+end
+
+function __get_ar_scale(ar::PeriodicARparameters, season::Int)
+    seasons = map(x -> x.season, ar.parameters)
+    index = findfirst(x -> x == season, seasons)
+    __get_ar_scale(ar.parameters[index])
+end
+
+function __get_ar_scale(uar::UnivariateAutoRegressive, season::Int)
+    __get_ar_scale(uar.parameters, season)
+end
+
+function __get_ar_scale(s::AutoRegressiveStochasticProcess, season::Int)
+    [__get_ar_scale(uar, season) for uar in s.signal_model]
+end
+
 function length(s::AutoRegressiveStochasticProcess)::Integer
     return length(s.signal_model)
 end
@@ -143,8 +189,100 @@ function __generate_saa(
     s::AutoRegressiveStochasticProcess,
     initial_season::Integer,
     N::Integer,
-    B::Integer)::Vector{Vector{Vector{Float64}}}
+    B::Integer)
 
     __generate_saa(rng, s.noise_model, initial_season, N, B)
     
+end
+
+function add_inflow_uncertainty!(m::JuMP.Model, s::AutoRegressiveStochasticProcess,
+    season::Int)
+
+    n_hydro = length(s)
+    max_lags = [__get_lag(i) for i in s.signal_model]
+    stchp_size = sum(max_lags)
+    
+    scales = __get_ar_scale(s, season)
+    inits = vcat([uar.initialization for uar in s.signal_model]...)
+
+    index_t = ones(Int,length(s))
+    for i in 1:(length(s) - 1)
+        index_t[i+1] = sum(max_lags[1:i]) + 1
+    end
+
+    st_mat = __build_state_transition_matrix(s, season, max_lags)
+    sel_mat = __build_selector_matrix(index_t, n_hydro)
+
+    m[ω_INFLOW] = @variable(m, [1:n_hydro], base_name = String(ω_INFLOW))
+    m[STCHP] = @variable(m,
+        [n = 1:stchp_size],
+        base_name = String(STCHP),
+        SDDP.State,
+        initial_value = inits[n])
+
+    @constraint(m, ar_model[n = 1:stchp_size], 
+        m[STCHP][n].out == st_mat[n,:]' * [var.in for var in m[STCHP]] + sel_mat[n,:]' * m[ω_INFLOW])
+    @constraint(m, inflow_model[n = 1:n_hydro],
+        m[INFLOW][n] .== m[STCHP][index_t[n]].out  * scales[n][2] + scales[n][1])
+
+    return m
+end
+
+function __arp2statematrix(phis::Vector{T} where T <: Real, size::Int)
+
+    out = zeros((size, size))
+
+    for i in 1:length(phis)
+        out[1, i] = phis[i]
+    end
+
+    for i in 2:size
+        out[i, i-1] = 1
+    end
+
+    return out
+
+end
+
+function __block_diagonal_matrix(matrices::Vector{Matrix{T}} where T <: Real)
+    
+    sizes = [size(m, 1) for m in matrices]
+    full_size = sum(sizes)
+    
+    offsets = zeros(Int, length(matrices))
+    for i in 1:(length(offsets) - 1)
+        offsets[i+1] = sum(sizes[1:i])
+    end
+
+    out = zeros(full_size, full_size)
+    for k in 1:length(matrices)
+        offset = offsets[k]
+        m = matrices[k]
+        for i in 1:sizes[k], j in 1:sizes[k]
+            out[offset + i, offset + j] = m[i, j]
+        end
+    end
+
+    return out
+end
+
+function __build_state_transition_matrix(s::AutoRegressiveStochasticProcess, season::Int,
+    max_lags::Vector{Int})
+
+    st_mat = __get_ar_parameters(s, season)
+    st_mat = [__arp2statematrix(m, l) for (m,l) in zip(st_mat, max_lags)]
+    st_mat = __block_diagonal_matrix(st_mat)
+    
+    return st_mat
+end
+
+function __build_selector_matrix(Is::Vector{Int}, J)
+    I = sum(Is)
+    sel_mat = zeros(Int, (I, J))
+
+    for (i,j) in zip(Is, 1:J)
+        sel_mat[i,j] = 1
+    end
+
+    return sel_mat
 end
