ad69be8a58f4b5e49cf04890e064510e7b4b4f2c
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-12-05 16:51:54 -0300

    Fixes error on stch-utils test

--- test/Utils/test-stochasticprocess-utils.jl
+++ test/Utils/test-stochasticprocess-utils.jl
@@ -2,10 +2,10 @@ import SDDPlab: Utils
 
 @testset "stochasticprocess-utils" begin
     @testset "node2season" begin
-        init_1 = [__node2season(i, 4, 1) for i in 1:8]
+        init_1 = [Utils.__node2season(i, 4, 1) for i in 1:8]
         @test all(init_1 .== [1,2,3,4,1,2,3,4])
         
-        init_2 = [__node2season(i, 4, 2) for i in 1:8]
+        init_2 = [Utils.__node2season(i, 4, 2) for i in 1:8]
         @test all(init_2 .== [2,3,4,1,2,3,4,1])
     end
 end
