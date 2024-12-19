6f02564c1e5f15d1b05f6532831846f9f0619e6d
Rogerio Alves <rogerioalves.ee@gmail.com>
2024-12-19 10:05:04 -0300

    adds logo and icon

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,18 @@
 using Documenter, SDDPlab
 
+format = Documenter.HTML(;
+    edit_link = "main",
+    prettyurls = get(ENV, "CI", nothing) == "true",
+    assets = [joinpath("assets", "favicon.ico")],
+)
+
 makedocs(;
-    pages = Any[
-        "Introduction" => "index.md",
-        "User Guide" => Any["Getting Started" => "man/getting_started.md"],
-    ],
+    # modules = [SDDPlab],
     sitename = "SDDPlab.jl",
+    format = format,
+    checkdocs = :exports,
+    pages = [
+        "Introduction" => "index.md",
+        "User Guide" => ["Getting Started" => "man/getting_started.md"],
+    ],
 )
